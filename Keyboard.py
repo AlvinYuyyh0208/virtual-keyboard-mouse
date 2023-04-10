ea11015e62fcaeb404d6a095914791cb533931dc
AlvinYuyyh0208 <130363237+AlvinYuyyh0208@users.noreply.github.com>
2023-04-10 19:39:54 +0800

    Add files via upload

---
+++ Keyboard.py
@@ -0,0 +1,137 @@
+import cv2
+import numpy as np
+
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+from pynput.keyboard import Controller, Key
+import mediapipe as mp
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+from threading import Thread
+from queue import Queue
+import random
+import time
+
+size = 1
+q = Queue(size)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8)
+
+# 控制设备音响硬件
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# print(volume.GetMasterVolumeLevel())
+# 音量范围
+volRan = volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(-1.0, None)
+
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "<--"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "UP"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "DOWN"]]
+finalText = ""
+Keyboard = Controller()
+
+
+# 绘制键盘
+# 此处顺序为BGR
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 0), cv2.FILLED)  # start, size, color
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)  # text, location, font, proportion, color, thickness
+
+    return img
+
+
+class Button():
+    def __init__(self, pos, text, size):
+        self.pos = pos
+        self.text = text
+        self.size = size
+
+
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        if key == '<--':
+            buttonList.append(Button([100 * j + 50, 100 * i + 50], key, (85 * 2, 85)))
+        if key == 'UP':
+            buttonList.append(Button([100 * j + 50, 100 * i + 50], key, (85 * 2, 85)))
+
+        if key == 'DOWN':
+            buttonList.append(Button([100 * j + 50, 100 * i + 50], key, (85 * 3, 85)))
+
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key, (85, 85)))
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
+
+    drawAll(img, buttonList)
+
+    # 判断手指是否在按钮上
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+            # 8号为右手食指指尖
+            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
+                cv2.rectangle(img, button.pos, (x + w, y + h), (160, 160, 160), cv2.FILLED)  # start, size, color
+                cv2.putText(img, button.text, (x + 20, y + 65),
+                            cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255),
+                            4)  # text, location, font, proportion, color, thickness
+
+                # 食指指尖和中指指尖的距离
+                l, _, _ = detector.findDistance(8, 12, img, draw=False)
+                print(l)
+
+                # 当点击的时候
+                if l < 30:
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (255, 178, 102), cv2.FILLED)  # start, size, color
+                    cv2.putText(img, button.text, (x + 20, y + 65),
+                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255),
+                                4)
+                    if button.text == '<--':
+                        Keyboard.press(Key.backspace)
+                        finalText = finalText[:-1]
+
+                    if button.text == 'UP':
+                        # 当前音量
+                        vol = volume.GetMasterVolumeLevel()
+
+                        volume.SetMasterVolumeLevel(vol + 1, None)
+
+                    if button.text == 'DOWN':
+                        vol = volume.GetMasterVolumeLevel()
+
+                        volume.SetMasterVolumeLevel(vol - 1, None)
+
+
+                    else:
+                        finalText += button.text
+                        Keyboard.press(button.text)
+            # 可更改
+            sleep(0.15)
+
+    # 绘制文本框
+    cv2.rectangle(img, (50, 350), (700, 450), (160, 160, 160), cv2.FILLED)  # start, size, color
+    cv2.putText(img, finalText, (60, 425), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
+
+# class ProducerThread(Thread):
+# def __int__(self,name=None):
